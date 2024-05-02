14ef6a49b0c36313bdebd1922d0855f4ac3f6ff3
Dannyel Cardoso da Fonseca <dannyelcf@gmail.com>
2024-05-02 09:37:36 +0200

    Refactor to use constant data

---
+++ exercises/convertTemperature/src/data/constants.js
@@ -0,0 +1,3 @@
+export const SPACE_REGEX = /\s+/;
+export const TEMPERATURES_INPUT = 'temperatures';
+export const CONVERT_TEMPERATURE_CONTAINER = 'convertedTemperatures';
--- exercises/convertTemperature/src/init.js
+++ exercises/convertTemperature/src/init.js
@@ -1,38 +1,44 @@
-debugger; // once when the program is initialized
+import {
+  SPACE_REGEX,
+  TEMPERATURES_INPUT,
+  CONVERT_TEMPERATURE_CONTAINER,
+} from './data/constants.js';
 
-document.getElementById('temperatures').addEventListener('change', (event) => {
-  debugger; // each time the user changes the 'temperatures' input
+document
+  .getElementById(TEMPERATURES_INPUT)
+  .addEventListener('change', (event) => {
+    //debugger; // each time the user changes the 'temperatures' input
 
-  const convertedTemperaturesContainer = document.getElementById(
-    'convertedTemperatures',
-  );
+    const convertedTemperaturesContainer = document.getElementById(
+      CONVERT_TEMPERATURE_CONTAINER,
+    );
 
-  // Erase previous content
-  convertedTemperaturesContainer.innerHTML = '';
+    // Erase previous content
+    convertedTemperaturesContainer.innerHTML = '';
 
-  // Get the text input
-  const fahrenheitTextList = event.target.value;
+    // Get the text input
+    const fahrenheitTextList = event.target.value;
 
-  // Validade it
-  if (fahrenheitTextList) {
-    if (!/^[0-9\s]*$/.test(fahrenheitTextList)) {
-      window.alert(
-        `The '${fahrenheitTextList}' contains values different of integer numbers`,
-      );
+    // Validade it
+    if (fahrenheitTextList) {
+      if (!/^[0-9\s]*$/.test(fahrenheitTextList)) {
+        window.alert(
+          `The '${fahrenheitTextList}' contains values different of integer numbers`,
+        );
+        return;
+      }
+
+      // Sanitize it
+      const fahrenheitList = fahrenheitTextList.trim().split(SPACE_REGEX);
+      fahrenheitList.forEach((fahrenheit) => {
+        // Do the math
+        const celsius = ((fahrenheit - 32) * 5) / 9;
+
+        // Render the result
+        const liString = `<li class="number-item">${celsius.toFixed(2)}</li>`;
+
+        convertedTemperaturesContainer.innerHTML =
+          convertedTemperaturesContainer.innerHTML + liString;
+      });
     }
-
-    // Sanitize it
-    const fahrenheitList = fahrenheitTextList.trim().split(/\s+/);
-
-    fahrenheitList.forEach((fahrenheit) => {
-      // Do the math
-      const celsius = ((fahrenheit - 32) * 5) / 9;
-
-      // Render the result
-      const liString = `<li class="number-item">${celsius.toFixed(2)}</li>`;
-
-      convertedTemperaturesContainer.innerHTML =
-        convertedTemperaturesContainer.innerHTML + liString;
-    });
-  }
-});
+  });
