c77f71e6a7cc6abce066365964b0e20b01fddd66
Pallavi Sarwar <pallavi.sarwar@gmail.com>
2024-05-06 15:23:26 +0200

    exercise-cap-swap-done

--- 6-refactoring/exercises/prompt-alert-confirm/cap-swap/src/listener.js
+++ 6-refactoring/exercises/prompt-alert-confirm/cap-swap/src/listener.js
@@ -1,1 +1,4 @@
+import { CAP_SWAP_BUTTON } from '../data/constants.js';
 import { capSwapHandler } from './handler.js';
+
+document.getElementById(CAP_SWAP_BUTTON).addEventListener('click', capSwapHandler);
