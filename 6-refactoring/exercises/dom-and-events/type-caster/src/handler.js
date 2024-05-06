ad51b9dfd334d6f071ab56a279e6ec2268824044
smita81 <smitabbsaraswat@gmail.com>
2024-05-06 14:28:39 +0200

    exercise-type-caster-done

--- 6-refactoring/exercises/dom-and-events/type-caster/src/handler.js
+++ 6-refactoring/exercises/dom-and-events/type-caster/src/handler.js
@@ -1,3 +1,31 @@
-import { typeCaster } from './utils.js';
+import { typeCaster } from './util.js';
 
-export const castTheValue = () => {};
+export const castTheValue = (event) => {
+    debugger;
+    // read & process user input
+    const form = event.target.form;
+    const intendedType = form.type.value;
+    const stringToCast = form.value.value;
+  
+    // execute core logic
+    let newValue;
+    if (intendedType === 'string') {
+      newValue = String(stringToCast);
+    } else if (intendedType === 'number') {
+      newValue = Number(stringToCast);
+    } else if (intendedType === 'boolean') {
+      newValue = Boolean(stringToCast);
+    } else {
+      newValue = undefined;
+    }
+  
+    // communicate result to user
+    document.getElementById(TYPE_CASTED_VALUE).innerHTML =
+      typeof newValue + ': ' + newValue;
+  
+    // log action for developers
+    console.log('\n-- user action --');
+    console.log('stringToCast:', stringToCast);
+    console.log('intendedType:', intendedType);
+    console.log('newValue:', newValue);
+  };
