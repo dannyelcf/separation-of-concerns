12bd456d3a89261f7494953e24bf6923c459489d
Pallavi Sarwar <pallavi.sarwar@gmail.com>
2024-05-06 10:57:09 +0200

    exercise-flip-book

--- 5-testing-components/exercises-write-components/game-board/game-board.js
+++ 5-testing-components/exercises-write-components/game-board/game-board.js
@@ -4,4 +4,20 @@
  * @param {Array[]} arrayOfArrays - a 2D array representing the game board
  * @returns {HTMLTableElement} the rendered game board
  */
-export const gameBoard = (arrayOfArrays) => {};
+export const gameBoard = (arrayOfArrays) => {
+    console.log(arrayOfArrays)
+    const table = document.createElement('table');
+
+    for (const array of arrayOfArrays){
+      console.log(array);
+      const tr = document.createElement('tr');
+        for (const element of array){
+            console.log(element);
+            const td = document.createElement('td');
+            td.innerText = element ;
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    return table;
+};
