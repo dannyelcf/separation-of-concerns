fef34c8a402795b0ff24b5e48fd138cdf206cbdb
smita81 <smitabbsaraswat@gmail.com>
2024-05-10 14:25:31 +0200

    modified-code

--- 6-refactoring/exercises/state/list-items/src/listener.js
+++ 6-refactoring/exercises/state/list-items/src/listener.js
@@ -1,5 +1,5 @@
 import { LIST_ALL_OF_THEM } from '../data/constants.js';
 import { listHandler } from './handler.js';
-import { list } from './util.js';
+
 
 document.getElementById(LIST_ALL_OF_THEM).addEventListener('click', listHandler);
